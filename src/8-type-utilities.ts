218c79d9e02439fb3bf8729f9e2dada998fb6325
Breno <breno.sena@hotmart.com>
2022-09-23 09:01:51 -0300

    feat: add more type utilities

--- src/8-type-utilities.ts
+++ src/8-type-utilities.ts
@@ -24,6 +24,30 @@ const todo3: Todo3 = {
   completed: true,
 };
 
+type Todo4 = Partial<Todo>;
+
+const todo4: Todo4 = {
+  title: 'Study back-end',
+};
+
+type Todo5 = Required<Todo4>;
+
+const todo5: Todo5 = {
+  title: 'Study React.js',
+  description: 'React.js is good!',
+  completed: true,
+};
+
+type Todo6 = Readonly<Todo>;
+
+const todo6: Todo6 = {
+  title: 'Study Vue.js',
+  description: 'Vue.js is good!',
+  completed: true,
+};
+
+//todo6.title = ''; Cannot assign to 'title' because it is a read-only property.
+
 type IconSize = {
   width: number;
   height: number;
@@ -40,3 +64,8 @@ const iconSizes: Record<IconSizeKey, IconSize> = {
 const getText = (text: string) => text;
 
 const data: ReturnType<typeof getText> = 'text';
+
+const theme = {
+  black: '#000',
+  white: '#fff',
+} as const;
